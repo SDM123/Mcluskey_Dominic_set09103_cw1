f0ca5742f87dc47e84d8a6d2c0ccdbafb63c238a
SDM123 <40277061@live.napier.ac.uk>
2018-10-15 12:21:02 +0100

    Made some real progress on coursework.

--- coursework.py
+++ coursework.py
@@ -3,7 +3,47 @@ app = Flask(__name__)
 
 @app.route('/')
 def root():
-   return render_template('cwbase.html'), 200
+   return render_template('cwTemplates/cwbase.html'), 200
+
+@app.route('/Nations')
+def nations():
+   return render_template('cwTemplates/cwnations.html')
+
+@app.route('/Nations/Africa')
+def africa():
+   return render_template('cwTemplates/cwafrica.html')
+
+@app.route('/Nations/Americas')
+def americas():
+   return render_template('cwTemplates/cwamericas.html')
+
+@app.route('/Nations/Asia')
+def asia():
+   return render_template('cwTemplates/cwasia.html')
+
+@app.route('/Nations/Europe')
+def europe():
+   return render_template('cwTemplates/cweurope.html')
+
+@app.route('/Nations/Oceania')
+def oceania():
+   return render_template('cwTemplates/cwoceania.html')
+
+@app.route('/Nations/Formable')
+def formable():
+   return render_template('cwTemplates/cwformable.html')
+
+@app.route('/Events')
+def events():
+   return render_template('cwTemplates/cwevents.html')
+
+@app.route('/Developer')
+def Developer():
+   return render_template('cwTemplates/cwdeveloper.html')
+
+@app.route('/Strategy')
+def Strategy():
+   return render_template('cwTemplates/cwstrategy.html')
 
 if __name__ == "__main__":
    app.run(host='0.0.0.0', debug=True)
