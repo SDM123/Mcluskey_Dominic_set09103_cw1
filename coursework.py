f89baaf1422fb8ca620698772e6dee2d09c73510
SDM123 <40277061@live.napier.ac.uk>
2018-10-20 20:40:44 +0100

    Finished coursework

--- coursework.py
+++ coursework.py
@@ -13,17 +13,49 @@ def nations():
 def africa():
    return render_template('cwTemplates/cwafrica.html')
 
+@app.route('/Nations/Africa/Mali')
+def mali():
+   return render_template('cwTemplates/cwmali.html')
+
+@app.route('/Nations/Africa/Kongo')
+def kongo():
+   return render_template('cwTemplates/cwkongo.html')
+
 @app.route('/Nations/Americas')
 def americas():
    return render_template('cwTemplates/cwamericas.html')
+
+@app.route('/Nations/Americas/Aztec')
+def aztec():
+   return render_template('cwTemplates/cwaztec.html')
+
+@app.route('/Nations/Americas/Huron')
+def huron():
+   return render_template('cwTemplates/cwhuron.html')
 
 @app.route('/Nations/Asia')
 def asia():
    return render_template('cwTemplates/cwasia.html')
 
+@app.route('/Nations/Asia/Ming')
+def ming():
+   return render_template('cwTemplates/cwming.html')
+
+@app.route('/Nations/Asia/Japan')
+def japan():
+   return render_template('cwTemplates/cwjapan.html')
+
 @app.route('/Nations/Europe')
 def europe():
    return render_template('cwTemplates/cweurope.html')
+
+@app.route('/Nations/Europe/France')
+def france():
+   return render_template('cwTemplates/cwfrance.html')
+
+@app.route('/Nations/Europe/Scotland')
+def scotland():
+   return render_template('cwTemplates/cwscotland.html')
 
 @app.route('/Nations/Oceania')
 def oceania():
@@ -32,6 +64,14 @@ def oceania():
 @app.route('/Nations/Formable')
 def formable():
    return render_template('cwTemplates/cwformable.html')
+
+@app.route('/Nations/Formable/Andalusia')
+def andalusia():
+   return render_template('cwTemplates/cwandalusia.html')
+
+@app.route('/Nations/Formable/Australia')
+def australia():
+   return render_template('cwTemplates/cwaustralia.html')
 
 @app.route('/Events')
 def events():
